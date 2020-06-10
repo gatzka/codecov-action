dd328627334eb1223bb70af0815208502b6f469b
Stephan Gatzka <stephan.gatzka@gin.de>
2020-06-10 11:13:40 +0200

    Make upload verbose.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 
-bash <(curl -s https://codecov.io/bash) -t $INPUT_TOKEN -s $INPUT_PATH -R $INPUT_ROOTPATH -p $INPUT_ROOTPATH
+bash <(curl -s https://codecov.io/bash) -v -t $INPUT_TOKEN -s $INPUT_PATH -R $INPUT_ROOTPATH -p $INPUT_ROOTPATH
 
